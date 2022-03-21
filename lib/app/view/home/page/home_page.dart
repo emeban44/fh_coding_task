e0c218376c58d9597706821d63e11bfbb58e6a78
emeban44 <emi.cazorla@hotmail.com>
2022-03-21 19:43:42 +0100

    all assets added as svg

--- lib/app/view/home/page/home_page.dart
+++ lib/app/view/home/page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,7 +7,46 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Lets go podba')),
-    );
+        appBar: AppBar(),
+        body: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 50),
+          child: Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(right: 5),
+                        child: SvgPicture.asset(
+                          'common_area.svg',
+                          width: MediaQuery.of(context).size.width * 0.02,
+                        ),
+                      ),
+                      Text(
+                        'Common area',
+                        style: TextStyle(
+                          fontSize: MediaQuery.of(context).size.width * 0.01,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text('CLICK'),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
